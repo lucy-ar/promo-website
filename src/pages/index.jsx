3a7d516c5d93aa571be1a0486942952ec0d4a6f8
[tina haibodi] <[tina.haibodi@queensu.ca]>
2019-02-11 18:22:11 -0500

    changing gradients on feature part

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -65,29 +65,29 @@ const Index = () => (
         <BigTitle>
           Meet, <br /> Lucy.
         </BigTitle>
-        <Subtitle>A creative mobile AR application that identifies spatial diameters, and renders interior content in real-time.</Subtitle>
+        <Subtitle>A creative mobile AR application that identifies spatial diameters, and renders 3-D interior content in real-time.</Subtitle>
       </Hero>
       <Projects offset={1}>
-        <Title>Features</Title>
+        <Title>Our Features</Title>
         <ProjectsWrapper>
           <ProjectCard
             title="Add + Drop"
             link="https://www.behance.net/gallery/58937147/Freiheit"
-            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
             Add and 
           </ProjectCard>
           <ProjectCard
             title="Harry Potter"
             link="https://www.behance.net/gallery/52915793/Harry-Potter"
-            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
             I entered the DOCMA 2017 award with this Harry Potter inspired image.
           </ProjectCard>
           <ProjectCard
             title="Tomb Raider"
             link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
           >
             Recreation of a Tomb Raider Wallpaper (Fan Art)
           </ProjectCard>
